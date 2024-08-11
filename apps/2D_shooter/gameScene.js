077120e1b4d8b86e877b17c97796f90975b0c013
Jimmy Ren <jr3582@drexel.edu>
2024-08-10 20:59:10 -0400

    Background imaging for scene

--- apps/2D_shooter/gameScene.js
+++ apps/2D_shooter/gameScene.js
@@ -6,53 +6,57 @@ class gameScene extends Phaser.Scene {
     }
 
     preload() {
+        // Load the bullet and background images
         this.load.image("bullet", "assets/bullet.png");
+        this.load.image("background", "assets/background.png"); // Load background image
     }
 
     create() {
+        // Add the background image, setting it to cover the entire game area
+        this.add.image(400, 300, "background").setDisplaySize(800, 600); 
+
         this.bulletGroup = new BulletGroup(this);
         this.addEvents();
 
-        //creating the player
+        // Create the player
         this.player = this.add.rectangle(400, 300, 50, 50, 0xff0000);
         this.physics.add.existing(this.player);
         this.player.body.setCollideWorldBounds(true);
 
-        //wall creation
+        // Wall creation
         this.walls = this.physics.add.staticGroup();
-        //this.walls.create(positionX, positionY, name)
         this.walls.create(200, 300, 'wall').setOrigin(0.5, 0.5).setDisplaySize(200, 20).refreshBody();
         this.walls.create(300, 400, 'wall').setOrigin(0.5, 0.5).setDisplaySize(20, 200).refreshBody();
 
-        //this part is for the movement (arrow keys)
+        // Set up movement (arrow keys)
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        //physics
+        // Set up physics
         this.physics.add.collider(this.player, this.walls);
 
-        //collision between obstacles and bullets
+        // Collision between obstacles and bullets
         this.physics.add.collider(this.walls, this.bulletGroup, (object1, object2) => {
             if (object2.state !== 1) {
                 object2.setActive(false);
                 object2.setVisible(false);
             }
         });
-
     }
-    
-    //mouseclick event for shooting
+
+    // Mouse click event for shooting
     addEvents() {
         this.input.on('pointerdown', pointer => {
             let bulletState = 3;
             this.shootBullet(pointer, bulletState);
-        })
+        });
     }
+
     shootBullet(pointer, bulletState) {
         this.bulletGroup.fireBullet(this.player, pointer.x, pointer.y, bulletState);
     }
 
     update() {
-        //moving the player
+        // Moving the player
         this.player.body.setVelocity(0);
 
         if(this.cursors.left.isDown) {
